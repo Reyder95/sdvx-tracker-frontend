ecdbf1f5da0a8ebe3e7df942d290c9f558d080c4
Konstantinos Houtas <reyd0905@gmail.com>
2020-04-29 13:21:25 -0400

    Modified the top of the song page slightly Modified the top of the song page to look a bit cleaner and look a bit better.

--- src/page-stuff/components/songview/songview_top.js
+++ src/page-stuff/components/songview/songview_top.js
@@ -3,23 +3,23 @@ import React from 'react'
 class SongViewTop extends React.Component {
     render() {
         return(
-            <div className="comp_songviewtop bg-tertiary">
+            <div className="comp_songviewtop bg-tertiary pb-4">
                 <div className="song-block">
                     <div className="song-image">
                         <h2 className="font-roboto-slab color-secondary mb-2 mt-2">かめりあ feat. ななひら - 超☆超☆光☆速☆出☆前☆最☆速!!! スピード★スター★かなで</h2>
                         <img src="https://placehold.it/128x128"/>
-                    </div>
+                        <div className="song-info font-source color-quartery">
+                            <p>
+                                <strong>BPM:</strong> 200 <br />
+                            </p>
+                            <p>
+                                <strong>Effector:</strong> Hirayasu Matsudo
+                            </p>
+                            <p>
+                                <strong>Game:</strong> SOUND VOLTEX II: Infinite Infection
+                            </p>
+                        </div>
 
-                    <div className="song-info font-roboto color-quartery mt-4">
-                        <p>
-                            <strong>BPM:</strong> xxx
-                        </p>
-                        <p>
-                            <strong>Effector:</strong> xxx
-                        </p>
-                        <p>
-                            <strong>Game:</strong> xxx
-                        </p>
                     </div>
                 </div>
             </div>
