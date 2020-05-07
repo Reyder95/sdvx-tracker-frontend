4c51359cf66ac716342bc04dae40d8e49bc9c467
Konstantinos Houtas <reyd0905@gmail.com>
2020-05-07 18:10:25 -0400

    Added type filtering

--- src/page-stuff/components/songlist/songtable.js
+++ src/page-stuff/components/songlist/songtable.js
@@ -12,6 +12,7 @@ class SongTable extends React.Component {
             search: '',
             level: 0,
             game: '',
+            type: '',
             next_page: '',
             page: 1,
             data: []
@@ -19,12 +20,12 @@ class SongTable extends React.Component {
     this.delayedCallback = _.debounce(this.handleSearchChange, 500)
     }
 
-    fetchApi(page, search, level, game) {
-        console.log('http://localhost:3000/api/songs?p=' + page + '&s=' + search + '&l=' + level + '&g=' + game)
+    fetchApi(page, search, level, game, type) {
+        console.log('http://localhost:3000/api/songs?p=' + page + '&s=' + search + '&l=' + level + '&g=' + game + '&t=' + type)
 
         let test = 0
 
-        axios.get('http://localhost:3000/api/songs?p=' + page + '&s=' + search + '&l=' + level + '&g=' + game)
+        axios.get('http://localhost:3000/api/songs?p=' + page + '&s=' + search + '&l=' + level + '&g=' + game + '&t=' + type)
             .then(res => {
                 const songs = res.data.data;
                 const nextPage = res.data.next_page;
@@ -63,25 +64,32 @@ class SongTable extends React.Component {
         const value = event.target.value
 
         this.setState({search: value})
-        this.fetchApi(1, value, this.state.level, this.state.game);
+        this.fetchApi(1, value, this.state.level, this.state.game, this.state.type);
     }
 
     handleGameChange(event) {
         const value = event.target.value
 
         this.setState({game: value})
-        this.fetchApi(1, this.state.search, this.state.level, value)
+        this.fetchApi(1, this.state.search, this.state.level, value, this.state.type)
     }
 
     handleLevelChange(event) {
         const value = event.target.value
 
         this.setState({level: value})
-        this.fetchApi(1, this.state.search, value, this.state.game)
+        this.fetchApi(1, this.state.search, value, this.state.game, this.state.type)
+    }
+
+    handleTypeChange(event) {
+        const value = event.target.value
+
+        this.setState({type: value})
+        this.fetchApi(1, this.state.search, this.state.level, this.state.game, value)
     }
 
     componentDidMount() {
-        this.fetchApi(this.state.page, this.state.search, this.state.level, this.state.game);
+        this.fetchApi(this.state.page, this.state.search, this.state.level, this.state.game, this.state.type);
     }
 
     refreshPage(next) {
@@ -92,7 +100,7 @@ class SongTable extends React.Component {
                 page: page
             })
 
-            this.fetchApi(page, this.state.search, this.state.level, this.state.game)
+            this.fetchApi(page, this.state.search, this.state.level, this.state.game, this.state.type)
         }
         else {
             let page = this.state.page - 1;
@@ -101,7 +109,7 @@ class SongTable extends React.Component {
                 page: page
             })
 
-            this.fetchApi(page, this.state.search, this.state.level, this.state.game)
+            this.fetchApi(page, this.state.search, this.state.level, this.state.game, this.state.type)
         }
     }
 
@@ -147,7 +155,7 @@ class SongTable extends React.Component {
                             </select>
                         </div>
                         <div className="form-flex-item column">
-                            <select id="type" className="form-input">
+                            <select onChange={this.handleTypeChange.bind(this)} id="type" className="form-input">
                                 <option value="" disabled selected hidden>Type</option>
                                 <option value="official">Official</option>
                                 <option value="custom">Custom</option>
