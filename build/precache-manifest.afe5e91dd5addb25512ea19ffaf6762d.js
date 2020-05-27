3dca56b69f998e2d83a67df9c97827785abf4f94
Konstantinos Houtas <reyd0905@gmail.com>
2020-05-27 18:45:50 -0400

    Added build folder

---
+++ build/precache-manifest.afe5e91dd5addb25512ea19ffaf6762d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74f312716f999e7d20c2e05fe2e5bb32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61238a23b0ddef605c2a",
+    "url": "/static/css/main.4a227728.chunk.css"
+  },
+  {
+    "revision": "8c69b32aac9f1acea276",
+    "url": "/static/js/2.2f64c538.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/static/js/2.2f64c538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61238a23b0ddef605c2a",
+    "url": "/static/js/main.ba731c54.chunk.js"
+  },
+  {
+    "revision": "112b35537fcfdfaade0c",
+    "url": "/static/js/runtime-main.54d26030.js"
+  }
+]);
